46460b6076d289af81d8e09a58fa31344377385d
pmc <pangmengchen@gmail.com>
2020-11-03 20:05:07 +0800

    feat: add hover pattern
    Signed-off-by: pmc <pangmengchen@gmail.com>

--- src/data.js
+++ src/data.js
@@ -8,12 +8,22 @@ export const libData = [
   },
   {
     id: '2',
-    libName:'hover',
+    libName:'magic',
     libMainClass:'magictime',
     classArr:["magic", "twisterInDown", "twisterInUp", "swap", "puffIn", "puffOut", "vanishIn", "vanishOut", "openDownLeft", "openDownRight", "openUpLeft", "openUpRight", "openDownLeftReturn", "openDownRightReturn", "openUpLeftReturn", "openUpRightReturn", "openDownLeftOut", "openDownRightOut", "openUpLeftOut", "openUpRightOut", "perspectiveDown", "perspectiveUp", "perspectiveLeft", "perspectiveRight", "perspectiveDownReturn", "perspectiveUpReturn", "perspectiveLeftReturn", "perspectiveRightReturn", "rotateDown", "rotateUp", "rotateLeft", "rotateRight", "slideDown", "slideUp", "slideLeft", "slideRight", "slideDownReturn", "slideUpReturn", "slideLeftReturn", "slideRightReturn", "swashOut", "swashIn", "foolishIn", "holeOut", "tinRightOut", "tinLeftOut", "tinUpOut", "tinDownOut", "tinRightIn", "tinLeftIn", "tinUpIn", "tinDownIn", "bombRightOut", "bombLeftOut", "boingInUp", "boingOutDown", "spaceOutUp", "spaceOutRight", "spaceOutDown", "spaceOutLeft", "spaceInUp", "spaceInRight", "spaceInDown", "spaceInLeft"]
   },
-  // {id: '3',libname:'magic'},
-  // {id: '4',libname:'pattern'},
+  {
+    id: '3',
+    libName:'hover',
+    libMainClass:'hover',
+    classArr: ["hvr-grow", "hvr-shrink", "hvr-pulse", "hvr-pulse-grow", "hvr-pulse-shrink", "hvr-push", "hvr-pop", "hvr-bounce-in", "hvr-bounce-out", "hvr-rotate", "hvr-grow-rotate", "hvr-float", "hvr-sink", "hvr-bob", "hvr-hang", "hvr-skew", "hvr-skew-forward", "hvr-skew-backward", "hvr-wobble-horizontal", "hvr-wobble-vertical", "hvr-wobble-to-bottom-right", "hvr-wobble-to-top-right", "hvr-wobble-top", "hvr-wobble-bottom", "hvr-wobble-skew", "hvr-buzz", "hvr-buzz-out", "hvr-forward", "hvr-backward", "hvr-fade", "hvr-back-pulse", "hvr-sweep-to-right", "hvr-sweep-to-left", "hvr-sweep-to-bottom", "hvr-sweep-to-top", "hvr-bounce-to-right", "hvr-bounce-to-left", "hvr-bounce-to-bottom", "hvr-bounce-to-top", "hvr-radial-out", "hvr-radial-in", "hvr-rectangle-in", "hvr-rectangle-out", "hvr-shutter-in-horizontal", "hvr-shutter-out-horizontal", "hvr-shutter-in-vertical", "hvr-shutter-out-vertical", "hvr-icon-back", "hvr-icon-forward", "hvr-icon-down", "hvr-icon-up", "hvr-icon-spin", "hvr-icon-drop", "hvr-icon-fade", "hvr-icon-float-away", "hvr-icon-sink-away", "hvr-icon-grow", "hvr-icon-shrink", "hvr-icon-pulse", "hvr-icon-pulse-grow", "hvr-icon-pulse-shrink", "hvr-icon-push", "hvr-icon-pop", "hvr-icon-bounce", "hvr-icon-rotate", "hvr-icon-grow-rotate", "hvr-icon-float", "hvr-icon-sink", "hvr-icon-bob", "hvr-icon-hang", "hvr-icon-wobble-horizontal", "hvr-icon-wobble-vertical", "hvr-icon-buzz", "hvr-icon-buzz-out", "hvr-border-fade", "hvr-hollow", "hvr-trim", "hvr-ripple-out", "hvr-ripple-in", "hvr-outline-out", "hvr-outline-in", "hvr-round-corners", "hvr-underline-from-left", "hvr-underline-from-center", "hvr-underline-from-right", "hvr-reveal", "hvr-underline-reveal", "hvr-overline-reveal", "hvr-overline-from-left", "hvr-overline-from-center", "hvr-overline-from-right", "hvr-shadow", "hvr-grow-shadow", "hvr-float-shadow", "hvr-glow", "hvr-shadow-radial", "hvr-box-shadow-outset", "hvr-box-shadow-inset", "hvr-bubble-top", "hvr-bubble-right", "hvr-bubble-bottom", "hvr-bubble-left","hvr-bubble-float-top","hvr-bubble-float-right","hvr-bubble-float-bottom","hvr-bubble-float-left","hvr-curl-top-left","hvr-curl-top-right","hvr-curl-bottom-right","hvr-curl-bottom-left"]
+  },
+  {
+    id: '4',
+    libname:'pattern',
+    libMainClass:'hover',
+    classArr:["pattern-checks-sm", "pattern-checks-md", "pattern-checks-lg", "pattern-checks-xl", "pattern-grid-sm", "pattern-grid-md", "pattern-grid-lg", "pattern-grid-xl", "pattern-dots-sm", "pattern-dots-md", "pattern-dots-lg", "pattern-dots-xl", "pattern-cross-dots-sm", "pattern-cross-dots-md", "pattern-cross-dots-lg", "pattern-cross-dots-xl", "pattern-diagonal-lines-sm", "pattern-diagonal-lines-md", "pattern-diagonal-lines-lg", "pattern-diagonal-lines-xl", "pattern-horizontal-lines-sm", "pattern-horizontal-lines-md", "pattern-horizontal-lines-lg", "pattern-horizontal-lines-xl", "pattern-vertical-lines-sm", "pattern-vertical-lines-md", "pattern-vertical-lines-lg", "pattern-vertical-lines-xl", "pattern-diagonal-stripes-sm", "pattern-diagonal-stripes-md", "pattern-diagonal-stripes-lg", "pattern-diagonal-stripes-xl", "pattern-horizontal-stripes-sm", "pattern-horizontal-stripes-md", "pattern-horizontal-stripes-lg", "pattern-horizontal-stripes-xl", "pattern-vertical-stripes-sm", "pattern-vertical-stripes-md", "pattern-vertical-stripes-lg", "pattern-vertical-stripes-xl", "pattern-triangles-sm", "pattern-triangles-md", "pattern-triangles-lg", "pattern-triangles-xl", "pattern-zigzag-sm", "pattern-zigzag-md", "pattern-zigzag-lg", "pattern-zigzag-xl"]
+},
   // {id: '5',libname:'csshake'},
 ]
 
@@ -25,7 +35,17 @@ export const libData = [
 // console.log(classArr)
 
 
-// 获取hover的类 https://www.minimamente.com/project/magic/
+// 获取magic的类 https://www.minimamente.com/project/magic/
 // const classArr = [];
 // [...document.querySelectorAll("#content .btn.btn-outline-primary")].forEach(x => classArr.push(x.dataset.test))
+// console.log(classArr)
+
+// 获取hover的类 http://ianlunn.github.io/Hover/
+// const classArr = [];
+// [...document.querySelectorAll("#effects a")].forEach(x => classArr.push(x.getAttribute('class')))
+// console.log(classArr)
+
+// 获取pattern的类 https://bansal.io/pattern-css#checks
+// const classArr = [];
+// [...document.querySelectorAll('.fs-s3.opacity-60 code')].forEach(x => classArr.push(x.innerText))
 // console.log(classArr)

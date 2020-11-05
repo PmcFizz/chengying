66fd7b0035386e25ac6e1e28c812799396db3e86
pmc <pangmengchen@gmail.com>
2020-11-05 17:56:18 +0800

    modify libnclass
    Signed-off-by: pmc <pangmengchen@gmail.com>

--- src/data.js
+++ src/data.js
@@ -21,13 +21,13 @@ export const libData = [
   {
     id: '4',
     libname: 'pattern',
-    libMainClass: 'hover',
+    libMainClass: 'pattern',
     classArr: ["pattern-checks-sm", "pattern-checks-md", "pattern-checks-lg", "pattern-checks-xl", "pattern-grid-sm", "pattern-grid-md", "pattern-grid-lg", "pattern-grid-xl", "pattern-dots-sm", "pattern-dots-md", "pattern-dots-lg", "pattern-dots-xl", "pattern-cross-dots-sm", "pattern-cross-dots-md", "pattern-cross-dots-lg", "pattern-cross-dots-xl", "pattern-diagonal-lines-sm", "pattern-diagonal-lines-md", "pattern-diagonal-lines-lg", "pattern-diagonal-lines-xl", "pattern-horizontal-lines-sm", "pattern-horizontal-lines-md", "pattern-horizontal-lines-lg", "pattern-horizontal-lines-xl", "pattern-vertical-lines-sm", "pattern-vertical-lines-md", "pattern-vertical-lines-lg", "pattern-vertical-lines-xl", "pattern-diagonal-stripes-sm", "pattern-diagonal-stripes-md", "pattern-diagonal-stripes-lg", "pattern-diagonal-stripes-xl", "pattern-horizontal-stripes-sm", "pattern-horizontal-stripes-md", "pattern-horizontal-stripes-lg", "pattern-horizontal-stripes-xl", "pattern-vertical-stripes-sm", "pattern-vertical-stripes-md", "pattern-vertical-stripes-lg", "pattern-vertical-stripes-xl", "pattern-triangles-sm", "pattern-triangles-md", "pattern-triangles-lg", "pattern-triangles-xl", "pattern-zigzag-sm", "pattern-zigzag-md", "pattern-zigzag-lg", "pattern-zigzag-xl"]
   },
   {
     id: '5',
     libName: 'csshake',
-    libMainClass: 'shake',
+    libMainClass: 'csshake',
     classArr: ["shake", "shake-little", "shake-slow", "shake-hard", "shake-horizontal", "shake-vertical", "shake-rotate", "shake-opacity", "shake-crazy", "shake-constant", "shake-chunk"],
   },
 ]
